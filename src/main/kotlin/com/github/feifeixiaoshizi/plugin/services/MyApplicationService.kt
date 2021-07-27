db609902f7894125062ec4c8c2a013d94ec36b29
GitHub Action <action@github.com>
2021-07-27 06:50:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/feifeixiaoshizi/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/feifeixiaoshizi/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.feifeixiaoshizi.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.feifeixiaoshizi.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/feifeixiaoshizi/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/feifeixiaoshizi/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.feifeixiaoshizi.plugin.services
 
+import com.github.feifeixiaoshizi.plugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
